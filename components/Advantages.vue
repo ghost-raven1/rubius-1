d00df5067187b9e3f1137470110f71e6e875fd17
ghost-raven1 <sonofagronom@gmail.com>
2021-03-12 02:07:37 +0700

    Добавлены наброски футера и контактов

--- components/Advantages.vue
+++ components/Advantages.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="section">
         <div class="container">
-          <h2>Преимущества</h2>
+          <h5>Добавить преимущества</h5>
         </div>
     </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,8 @@
 
      <Portfolio />
 
+     <Brands />
+
      <BtnToTop />
 
      <Contacts />
