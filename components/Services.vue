b21bca74804ef5c8a1bcf28566aba98fbb7bf08b
ghost-raven1 <sonofagronom@gmail.com>
2021-03-14 16:30:24 +0700

    Исправление ошибок

--- components/Services.vue
+++ components/Services.vue
@@ -2,37 +2,7 @@
     <section class="section">
         <div class="container">
           <h2>Популярные услуги</h2>
-          <div class="tabs">
-    <div class="tabs__nav">
-      <a class="tabs__link tabs__link_active" href="#content-1">Стрижка и укладка</a>
-      <a class="tabs__link" href="#content-2">Косметология</a>
-      <a class="tabs__link" href="#content-3">Маникюр и педикюр</a>
-      <a class="tabs__link" href="#content-4">Макияж</a>
-      <a class="tabs__link" href="#content-5">Брови и ресницы</a>
-      <a class="tabs__link" href="#content-6">Массаж</a>
-    </div>
-    <div class="tabs__content">
-      <div class="tabs__pane tabs__pane_show" id="content-1">
-        Содержимое 1...
-      </div>
-      <div class="tabs__pane" id="content-2">
-        Содержимое 2...
-      </div>
-      <div class="tabs__pane" id="content-3">
-        Содержимое 3...
-      </div>
-      <div class="tabs__pane" id="content-4">
-        Содержимое 4...
-      </div>
-      <div class="tabs__pane" id="content-5">
-        Содержимое 5...
-      </div>
-      <div class="tabs__pane" id="content-6">
-        Содержимое 6...
-      </div>
-      </div>
-    </div>
-  </div>
+        </div>
     </section>
 </template>
 
